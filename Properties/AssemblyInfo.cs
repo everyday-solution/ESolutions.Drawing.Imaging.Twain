fc9c3781109850aaf08731fd879bb459fd9f5021
Tobias Mundt <tobias.mundt@outlook.com>
2020-02-18 01:42:09 +0100

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle ("ESolutions.Drawing.Imaging.Twain")]
+[assembly: AssemblyDescription ("TWAIN control to acquire images from TWAIN sources")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Tobias Mundt & Partner - everyday Solutions")]
+[assembly: AssemblyProduct ("ESolutions.Drawing.Imaging.Twain")]
+[assembly: AssemblyCopyright ("2006 Tobias Mundt & Partner - everyday Solutions")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible (false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid ("c42097a1-baf2-4adb-8686-a9a23e6d83a8")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
